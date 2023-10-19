0fa69c5733ad365ca42167f405750f8a4c1a0477
lebang11 <lebangnong@gmail.com>
2023-10-19 19:47:28 +0200

    added bootstrap to react app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+        <div className="container">
+          <h1>This is Bootstrap</h1>
+        </div>
     </div>
   );
 }
