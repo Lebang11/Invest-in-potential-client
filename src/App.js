18742b9f3d785dc9af66eb6fb61d86fb624c0371
lebang11 <lebangnong@gmail.com>
2023-10-19 20:09:16 +0200

    more css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
     {/* navbar */}
-    <nav class="navbar navbar-light bg-light mb-5">
-      <span class="navbar-brand mb-0 h1">Navbar</span>
+    <nav class="navbar navbar-light bg-light mb-5 justify-content-center align-items-center">
+      <span class="navbar-brand  h1">Investing in Potential</span>
     </nav>
 
     {/*  simple start of table */}
     
         <div className="container">
+          <h1 className="display-4 text-muted">
+            Members
+          </h1>
           <table className="table table-light table-striped-columns">
             <thead>
               <tr>
@@ -18,7 +22,7 @@ function App() {
                 <th scope='col'>Name</th>
                 <th scope='col'>Surname</th>
                 <th scope='col'>Phone Number</th>
-                <th scope='col'>Points</th>
+                <th className='text-info' scope='col'>Points</th>
               </tr>
             </thead>
             <tbody>
