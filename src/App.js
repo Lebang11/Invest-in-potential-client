add546756f9307314f7ac45ee45220b9204ed5f8
lebang11 <lebangnong@gmail.com>
2023-10-19 20:00:02 +0200

    started table

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import './App.css';
 
 function App() {
   return (
+    // simple start of table
     <div className="App">
         <div className="container">
-          <h1>This is Bootstrap</h1>
+          <table className="table table-light">
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Name</th>
+                <th scope='col'>Surname</th>
+                <th scope='col'>Phone Number</th>
+                <th scope='col'>Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Lebang</td>
+                <td>Nong</td>
+                <td>0623997680</td>
+                <td>4</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
     </div>
   );
