9b07c2e2daa0878dbb3e10ef58c4decf98bf4518
L.Nong <lnong023@student.wethinkcode.co.za>
2023-11-08 17:37:16 +0200

    gallery has modal

--- src/picture.js
+++ src/picture.js
@@ -24,12 +24,35 @@ const Picture = (props) => {
 
         getPictures(props.imagename)
     }, [])
-    return ( 
+    return (
         <div class="col-lg-3 col-md-4 col-6">
-           <a href="#" class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" src={imageDownload} alt=""/>
-            </a> 
-        </div>
+            <div>
+            <a type="button" class="d-block mb-4 h-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <img class="img-fluid img-thumbnail" src={imageDownload} alt=""/>
+                </a> 
+            </div>
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content bg-light">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">{props.imagename}</h5>
+                        <button type="button" class="close btn" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body ">
+                        <img class="img-fluid img-thumbnail" src={imageDownload} alt=""/>
+                        
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        {/* <button type="button" class="btn btn-primary">Save changes</button> */}
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        
         
      );
 }
