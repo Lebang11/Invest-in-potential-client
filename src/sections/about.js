2f0cc1b3cf876306b144c677ca2d03cfac325121
Lebang Nong <lebangnong@gmail.com>
2024-09-18 14:41:18 +0200

    fontsize change and fixes

--- src/sections/about.js
+++ src/sections/about.js
@@ -18,9 +18,11 @@ const About = () => {
                     <img src={'gallery/A1D90854-2ED1-4714-BBA9-4D3B3D19D155.jpeg'} className=' m-3 w-100 rounded mw-100'></img>
                   </div>
                 </div>
-                <div className='col-sm-6 '>
-                  <div className='container ' >
-                    <p className="mx-3 text-center ">Investing in you to help you invest in yourself. <br></br> 
+                <div className='col-sm-6 d-flex justify-content-center align-items-center'>
+                  <div className='container' >
+                    <p className="mx-3 text-center " style={{
+                      fontSize: "0.7rem"
+                    }}>Investing in you to help you invest in yourself. <br></br> 
                     Welcome to "Investing in Yourself Inc.", where our mission is to empower you for comprehensive growth across multiple dimensions – cognitive, emotional, ethical, spiritual, physical, existential, and, naturally, financial. Embark on a transformative journey with us as we assist individuals and entrepreneurs in navigating a multidimensional path to success.</p>
                     <a class="d-flex btn btn-outline-secondary justify-content-center mb-5" data-bs-toggle="collapse" href="#about-more" role="button" aria-expanded="false" aria-controls="about-more" onClick={() => {
                       if (showMessage === "Show more") {
